15db99a55c3366fa8c761603673f460e9deda7c0
Andres Salas Olsen <asalaso@ucenfotec.ac.cr>
2018-06-20 16:04:42 -0600

    landing page

--- src/main/webapp/app/green-life/green-life.module.ts
+++ src/main/webapp/app/green-life/green-life.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 import { NavbarGreenlifeComponent } from './layout/navbar-greenlife/navbar-greenlife.component';
 import { FooterGreenlifeComponent } from './layout/footer-greenlife/footer-greenlife.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatGridListModule
   ],
   declarations: [LandingComponent, NavbarGreenlifeComponent, FooterGreenlifeComponent]
 })
