ef18df76c628d68e0c03b9e74072364461055403
Unknown <joshua.segura21@gmail.com>
2018-06-20 11:04:57 -0600

    Se agrega el navbar de Green Life y la fuente Lato
    Se agrega el navbar de Green Life y la fuente Lato. 
    
    También se añadio al global.scss los modulos necesarios de angular material, y la configuración de las fuentes en los componentes de Angular Material.

--- src/main/webapp/app/green-life/green-life.module.ts
+++ src/main/webapp/app/green-life/green-life.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 import { NavbarGreenlifeComponent } from './layout/navbar-greenlife/navbar-greenlife.component';
 import { FooterGreenlifeComponent } from './layout/footer-greenlife/footer-greenlife.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatMenuModule, MatButtonModule, MatButtonToggleModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatToolbarModule
   ],
   declarations: [LandingComponent, NavbarGreenlifeComponent, FooterGreenlifeComponent]
 })
