1cb0e2ec7fd2c6dfbc83f016787454fda3773711
Unknown <joshua.segura21@gmail.com>
2018-06-23 16:28:13 -0600

    Se arreglan las importaciones de los componentes

--- src/main/webapp/app/green-life/green-life.module.ts
+++ src/main/webapp/app/green-life/green-life.module.ts
@@ -1,5 +1,4 @@
 import { MatButtonModule, MatButtonToggleModule, MatCardModule, MatGridListModule, MatListModule, MatMenuModule, MatToolbarModule } from '@angular/material';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
@@ -9,8 +8,7 @@ import { NavbarGreenlifeComponent } from './layout/navbar-greenlife/navbar-green
 import { NgModule } from '@angular/core';
 import { RootComponent } from './root/root.component';
 import { RouterModule } from '@angular/router';
-import { FooterGreenlifeComponent } from './layout/footer-greenlife/footer-greenlife.component';
-import { MatMenuModule, MatButtonModule, MatButtonToggleModule, MatToolbarModule, MatCardModule, MatGridListModule, MatListModule } from '@angular/material';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
