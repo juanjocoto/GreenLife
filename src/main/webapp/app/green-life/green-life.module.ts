43fafe6c17b764485bf2229642de8a49014f0e25
Andres Salas Olsen <asalaso@ucenfotec.ac.cr>
2018-06-24 00:13:47 -0600

    change module

--- src/main/webapp/app/green-life/green-life.module.ts
+++ src/main/webapp/app/green-life/green-life.module.ts
@@ -1,20 +1,33 @@
-import { MatButtonModule, MatButtonToggleModule, MatCardModule, MatGridListModule,
-  MatListModule, MatMenuModule, MatToolbarModule, MatDialogModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatGridListModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatToolbarModule,
+  MatDialogModule
+} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { FooterGreenlifeComponent } from './layout/footer-greenlife/footer-greenlife.component';
 import { LandingComponent } from './views/landing/landing.component';
 import { NavbarGreenlifeComponent } from './layout/navbar-greenlife/navbar-greenlife.component';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RootComponent } from './root/root.component';
 import { RouterModule } from '@angular/router';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './dialogos/login/login.component';
 
 @NgModule({
   imports: [
+    RouterModule,
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
@@ -26,12 +39,23 @@ import { LoginComponent } from './dialogos/login/login.component';
     MatCardModule,
     MatGridListModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     MatDialogModule,
-    FormsModule,
-    RouterModule
+    FormsModule
   ],
-  declarations: [LandingComponent, NavbarGreenlifeComponent, FooterGreenlifeComponent,
-    RootComponent, LoginComponent],
-  entryComponents: [LoginComponent]
+  declarations: [
+    LandingComponent,
+    NavbarGreenlifeComponent,
+    FooterGreenlifeComponent,
+    RootComponent,
+    LoginComponent
+  ],
+  entryComponents: [
+    LoginComponent
+  ]
 })
 export class GreenLifeModule { }
