0dc9d30e9c048c1b4e77c21a297767be36a2807f
Joshua Segura <joshua.segura21@gmail.com>
2018-07-10 21:35:22 -0600

    Se carga el navbar dependiendo del rol de usuario

--- src/main/webapp/app/green-life/layout/navbar-greenlife/navbar-greenlife.component.ts
+++ src/main/webapp/app/green-life/layout/navbar-greenlife/navbar-greenlife.component.ts
@@ -1,62 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import { ActivatedRoute, Router, RoutesRecognized } from '@angular/router';
-import { AccountService, LoginService } from '../../../shared';
-import { LoginComponent } from '../../dialogos/login/login.component';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {ActivatedRoute, Router, RoutesRecognized} from '@angular/router';
+import {AccountService, LoginService, User, UserService} from '../../../shared';
+import {LoginComponent} from '../../dialogos/login/login.component';
+import {HttpResponse} from '@angular/common/http';
 
 @Component({
-  selector: 'jhi-navbar-greenlife',
-  templateUrl: './navbar-greenlife.component.html',
-  styleUrls: ['navbar-greenlife.scss']
+    selector: 'jhi-navbar-greenlife',
+    templateUrl: './navbar-greenlife.component.html',
+    styleUrls: ['navbar-greenlife.scss']
 })
 export class NavbarGreenlifeComponent implements OnInit {
 
-  collapse = false;
-  configuracion = true;
-  isAuth = true;
-  usuarioId = '';
+    collapse = false;
+    configuracion = true;
+    isAuth = true;
+    usuarioId = '';
+    currentUser: User;
 
-  constructor(private dialog: MatDialog, private router: Router,
-    private loginService: LoginService, private auth: AccountService,
-    private route: ActivatedRoute) {
-      this.verificarSesion();
-      if (!this.route.firstChild.data['value']['configuracion']) {
-        this.configuracion = false;
-      }
+    constructor(private dialog: MatDialog,
+                private router: Router,
+                private loginService: LoginService,
+                private auth: AccountService,
+                private userService: UserService,
+                private route: ActivatedRoute) {
+        this.verificarSesion();
+        if (!this.route.firstChild.data['value']['configuracion']) {
+            this.configuracion = false;
+        }
     }
 
-  ngOnInit() {
-    this.router.events.subscribe((event) => {
-      console.log(event);
-      if (event instanceof RoutesRecognized) {
-        const data = event.state.root.firstChild.firstChild.data;
-        this.configuracion = data.configuracion as boolean;
-        this.verificarSesion();
-      }
-    });
-  }
+    ngOnInit() {
+        this.router.events.subscribe((event) => {
+            console.log(event);
+            if (event instanceof RoutesRecognized) {
+                const data = event.state.root.firstChild.firstChild.data;
+                this.configuracion = data.configuracion as boolean;
+                this.verificarSesion();
+            }
+        });
+    }
 
-  private verificarSesion() {
-    this.auth.get().subscribe((resul) => {
-      this.isAuth = true;
-      this.usuarioId = resul.body['login'];
-    }, () => {
-      this.isAuth = false;
-    });
-  }
+    private verificarSesion() {
+        this.auth.get().subscribe((resul) => {
+            this.isAuth = true;
+            this.usuarioId = resul.body['login'];
+            this.loadCurrentUser(resul.body['login']);
+        }, () => {
+            this.isAuth = false;
+        });
+    }
 
-  toggleCollapse() {
-    this.collapse = !this.collapse;
-  }
+    toggleCollapse() {
+        this.collapse = !this.collapse;
+    }
 
-  openDialog() {
-    this.dialog.open(LoginComponent, {
-      width: '300px'
-    });
-  }
+    openDialog() {
+        this.dialog.open(LoginComponent, {
+            width: '300px'
+        });
+    }
 
-  cerrarSesion() {
-    this.loginService.logout();
-    this.router.navigate(['/']);
-  }
+    cerrarSesion() {
+        this.loginService.logout();
+        this.router.navigate(['/']);
+    }
+
+    private loadCurrentUser(login) {
+        this.userService.find(login).subscribe((userResponse: HttpResponse<User>) => {
+            this.currentUser = userResponse.body;
+            console.log(userResponse.body);
+        });
+    }
 }
